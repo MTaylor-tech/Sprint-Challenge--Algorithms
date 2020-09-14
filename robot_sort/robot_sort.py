ff631267db8a2ef48db75f167874b36c19de91e5
Matthew Taylor <majikpig@gmail.com>
2020-09-13 20:46:26 -0400

    robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,7 +96,21 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
+        if self.can_move_right():
+            self.move_right()
+            self.swap_item()
+            self.move_left()
+            while self.compare_item() < 0 and self.can_move_left():
+                self.move_left()
+            if self.compare_item() < 0:
+                self.swap_item()
+            while self.can_move_right() and self.compare_item() is not None:
+                self.move_right()
+                self.swap_item()
+            self.sort()
+
+
+    def down_the_pass(self):
         pass
 
 
